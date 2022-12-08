41df17e99204f2f8d6d70da4867688bb9ea84254
Mehmet Salgar <salgarm@gmx.de>
2022-12-08 16:25:48 +0100

    ArgoCD Trials  - FSM Akk 4 Eyes Application Deployment  - Without Infrastructure  - Nexus with Traefik  - Addres Check

---
+++ address-check-application/src/main/java/org/salgar/akka/fsm/foureyes/addresscheck/AddressCheckApplication.java
@@ -0,0 +1,13 @@
+package org.salgar.akka.fsm.foureyes.addresscheck;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class AddressCheckApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .registerShutdownHook(true)
+                .run(args);
+    }
+}
